5edaf562e301ef397d0b95b3f0bf7114aa182b13
byseven <2104898527@qq.com>
2025-08-21 19:05:09 +0800

    refactor(ui): 移除多余的WebView构造函数

--- app/src/main/java/fansirsqi/xposed/sesame/ui/MyWebView.kt
+++ app/src/main/java/fansirsqi/xposed/sesame/ui/MyWebView.kt
@@ -10,45 +10,17 @@ import android.webkit.WebViewClient
  * 自定义 WebView 类，提供默认的初始化设置和滚动到底部的功能。
  */
 class MyWebView : WebView {
-    /**
-     * 构造函数，用于当没有 AttributeSet 参数时。
-     *
-     * @param c Context 对象
-     */
+
     constructor(c: Context) : super(c) {
         defInit()
     }
 
-    /**
-     * 构造函数，用于当有 AttributeSet 参数时。
-     *
-     * @param context Context 对象
-     * @param attrs   AttributeSet 对象
-     */
     constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
         defInit()
     }
 
-    /**
-     * 构造函数，用于当有 AttributeSet 和 defStyleAttr 参数时。
-     *
-     * @param context     Context 对象
-     * @param attrs       AttributeSet 对象
-     * @param defStyleAttr 默认样式属性
-     */
-    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
-        defInit()
-    }
 
-    /**
-     * 构造函数，用于当有 AttributeSet、defStyleAttr 和 defStyleRes 参数时。
-     *
-     * @param context    Context 对象
-     * @param attrs      AttributeSet 对象
-     * @param defStyleAttr 默认样式属性
-     * @param defStyleRes 默认样式资源
-     */
-    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes) {
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
         defInit()
     }
 
@@ -91,4 +63,5 @@ class MyWebView : WebView {
         // 计算垂直滚动范围并滚动到底部
         scrollTo(0, computeVerticalScrollRange())
     }
+
 }

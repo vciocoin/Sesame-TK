1d167c6da60a3bad8d4714537ef72c2ca6d5fb6b
byseven <2104898527@qq.com>
2025-08-25 17:07:19 +0800

    feat: 添加Xposed服务监听和信息记录

--- app/src/main/java/fansirsqi/xposed/sesame/hook/LspEntry.kt
+++ app/src/main/java/fansirsqi/xposed/sesame/hook/LspEntry.kt
@@ -2,9 +2,10 @@ package fansirsqi.xposed.sesame.hook
 
 import de.robv.android.xposed.XposedBridge
 import fansirsqi.xposed.sesame.data.General
-import fansirsqi.xposed.sesame.data.ViewAppInfo
 import io.github.libxposed.api.XposedModule
 import io.github.libxposed.api.XposedModuleInterface
+import io.github.libxposed.service.XposedService
+import io.github.libxposed.service.XposedServiceHelper
 
 
 class LspEntry(
@@ -14,12 +15,36 @@ class LspEntry(
     var customHooker: ApplicationHook? = null
     private val processName = param.processName
 
+    object XposedInfo {
+        var apiVersion: Int = 0
+        var frameworkName: String = ""
+        var frameworkVersion: String = ""
+        var frameworkVersionCode: Long = 0
+        var scope: MutableList<String?> = mutableListOf()
+
+    }
+
     init {
+        XposedServiceHelper.registerListener(object : XposedServiceHelper.OnServiceListener {
+            override fun onServiceBind(service: XposedService) {
+                XposedInfo.apiVersion = service.apiVersion
+                XposedInfo.frameworkName = service.frameworkName
+                XposedInfo.frameworkVersion = service.frameworkVersion
+                XposedInfo.frameworkVersionCode = service.frameworkVersionCode
+                XposedInfo.scope = service.scope
+                XposedBridge.log("Service bound: ${service.frameworkName} ${service.frameworkVersion}")
+            }
+
+            override fun onServiceDied(service: XposedService) {
+                XposedBridge.log("Service died: ${service.frameworkName}")
+            }
+        })
+
         XposedBridge.log("LspEntry: Initialized for process $processName")
-        val frameWorkName = base.frameworkName
-        val frameworkVersion = base.frameworkVersion
-        ViewAppInfo.xpFrameworkVersion = "$frameWorkName $frameworkVersion"
-        XposedBridge.log("LspEntry: Framework name: $frameWorkName, version: $frameworkVersion")
+        val baseFw = "${base.frameworkName} ${base.frameworkVersion}"
+        val fw = "${XposedInfo.frameworkName} ${XposedInfo.frameworkVersion}"
+        XposedBridge.log("Current framework: $fw, API version: ${XposedInfo.apiVersion}")
+        XposedBridge.log("LspEntry: Framework from base: $baseFw ")
     }
 
     override fun onPackageLoaded(param: XposedModuleInterface.PackageLoadedParam) {
@@ -30,4 +55,6 @@ class LspEntry(
             customHooker?.loadModelPackage(param)
         }
     }
+
+
 }
